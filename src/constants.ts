87b9e2c15e949933a89121666a89636ddf87bb3d
adz <x12@adz.garden>
2024-05-12 13:40:52 +0200

    Update schemas after change in p2panda data types

--- src/constants.ts
+++ src/constants.ts
@@ -1,16 +1,16 @@
 // Identifiers of the schemas we use
 export const EVENTS_SCHEMA_ID =
-  'events_002068630c898d13851d24f99f2671d2a5170e8fe5dbccc5da8415884a4bcc0622aa';
+  'events_002059d5a03b7fe7b1c9c5c752146e25f354b39d23a31e31dd6a5b3ecb25c28e71ed';
 export const PROFILES_SCHEMA_ID =
-  'profiles_0020a895b47a1319005369800f9e9fb9d74c54569e3e50610aa4c0a4065290e6ce19';
+  'profiles_00200ce1565eb24a1c1c3eadff919d9b8e2376870f93285228eb5e4a0a3a5777029c';
 export const REACTIONS_SCHEMA_ID =
-  'reactions_0020610e865e0dd7475201fc45fef59415fd0096c309932d53455c4a29918d174726';
+  'reactions_0020186d7a7f24f478357be25684c0a9c019a595671762a8cf71b2ca72332b9defc1';
 export const COMMENTS_SCHEMA_ID =
-  'comments_0020451cc328dc2b519a2a2ad7e9959f27c14c1b2072dab2937c3e341d7281c4eea1';
+  'comments_0020412850392f504a2ed54a780bedf2d7807f067dc7b47fbe14aa37ed9784276503';
 
 // URL of your local aquadoggo node
 export const ENDPOINT = 'http://localhost:2020/graphql';
 
 // Time-range of the unconference event
-export const MIN_DATE = '2023-07-28';
-export const MAX_DATE = '2023-07-30';
+export const MIN_DATE = '2024-05-24';
+export const MAX_DATE = '2024-05-26';
